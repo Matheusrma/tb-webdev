a2f3389d7c52b36c03b8e4884fa7c9af4c347e3b
Matheus Motta <matheusmotta.mm@gmail.com>
2014-09-15 20:41:39 -0300

    Added route transitions

--- routing.js
+++ routing.js
@@ -1,17 +1,35 @@
 //ROUTING
 
-var login = function () { console.log("login"); };
-var register = function () { console.log("register"); };
+var login = function () { 
+  
+};
+
+var register = function () { 
+  
+};
+
+var allroutes = function() {
+  var route = window.location.hash.slice(2);
+  console.log(route);
+  var sections = $('section');
+  var section;
+
+  section = sections.filter('[data-route=' + route + ']');
+
+  if (section.length) {
+    sections.hide(250);
+    section.show(250);
+  }
+};
 
 var routes = {
   '/login': login,
-  '/register': [register, function() {
-    console.log("An inline route handler.");
-  }],
+  '/register': register,
 };
 
 var options = {
-  html5history : true
+  html5history : true,
+  on: allroutes
 }
 
 var router = Router(routes).configure(options);
